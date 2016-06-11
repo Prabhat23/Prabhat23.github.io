317ff4343eacb28d1a0de29485e9e7f2dff37d08
Prabhat23 <prabhatgangwar5051@gmail.com>
2016-06-11 15:15:28 +0530

    ejs example

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
  */
 var express = require('express')
 var app = express();
-app.get('/',function (req,res){
-    res.writeHead(200,{"contentType":"text/html"})
-    res.write("hello");
+app.get('/LOGIN',function (req,res){
+  res.render("login.ejs")
     res.end("")
 })
 app.listen(8080);
